32472729c77df475d7c734505c89034d930b0198
sabila111 <sharminsharker2002@gmail.com>
2025-01-03 20:39:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('Volunteer page is available')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Volunteer page is running on port ${port}`)
+  })
